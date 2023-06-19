74b23f55fcd082d6363d1b4e79708a81e91ca490
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-06-19 09:43:49 -0600

    added the rest of the tabs to the nav. going to add text next.

--- src/header.js
+++ src/header.js
@@ -5,7 +5,15 @@ header.appendChild(nav);
 
 const home = document.createElement('div');
 home.classList.add('tab');
+home.id = 'homeTab';
+nav.appendChild(home);
+
 const menu = document.createElement('div');
 menu.classList.add('tab');
+menu.id = 'menuTab';
+nav.appendChild(menu);
+
 const about = document.createElement('div');
 about.classList.add('tab');
+about.id = 'aboutTab';
+nav.appendChild(about);

5047a0081c50693cdb19f3f3db9638144483d898
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-06-20 20:07:03 -0600

    had ChatGPT write a menu for me.

--- src/mainContent.js
+++ src/mainContent.js
@@ -1,1 +1,37 @@
 const mainContentContainer = document.getElementById('content');
+
+// BREAKFAST WAFFLES
+
+// Classic Waffle - Freshly baked golden waffle served with butter and maple syrup.
+// Belgian Waffle - Light and fluffy Belgian waffle topped with fresh berries and whipped cream.
+// Chocolate Lover's Waffle - Decadent chocolate-infused waffle drizzled with chocolate sauce, sprinkled with powdered sugar, and served with a side of vanilla ice cream.
+// Cinnamon Roll Waffle - Mouthwatering waffle infused with cinnamon and topped with cream cheese frosting and caramelized pecans.
+// Fruity Blast Waffle - Waffle loaded with a medley of seasonal fruits, such as strawberries, blueberries, and bananas, accompanied by a dollop of Greek yogurt and a drizzle of honey.
+// Savory Breakfast Waffle - Savory waffle topped with scrambled eggs, crispy bacon or sausage, cheddar cheese, and a touch of hot sauce.
+// SWEET CREATIONS
+
+// Nutella Paradise Waffle - Irresistible waffle smeared with Nutella, sliced bananas, and crushed hazelnuts, finished with a dusting of powdered sugar.
+// Red Velvet Waffle - Rich red velvet waffle with cream cheese frosting, garnished with chocolate shavings.
+// S'mores Waffle - Toasted marshmallows, graham cracker crumbs, and melted chocolate drizzled over a warm waffle, reminiscent of the classic campfire treat.
+// Apple Pie Waffle - Waffle infused with apple pie spices, topped with warm cinnamon apples, a scoop of vanilla ice cream, and caramel sauce.
+// Peanut Butter & Jelly Waffle - Waffle spread with creamy peanut butter and your choice of strawberry or grape jelly, served with a sprinkle of crushed peanuts.
+// SAVORY DELIGHTS
+
+// Chicken & Waffles - Crispy fried chicken served with a fluffy waffle and a side of maple syrup or savory gravy.
+// Waffle Benedict - Poached eggs, ham or smoked salmon, and hollandaise sauce served atop a savory waffle.
+// Caprese Waffle - Tomato, fresh mozzarella, and basil layered on a savory waffle, drizzled with balsamic glaze.
+// BBQ Pulled Pork Waffle - Tender pulled pork smothered in tangy barbecue sauce, served on a waffle with coleslaw and pickles.
+// SIDES
+
+// Crispy Bacon
+// Scrambled Eggs
+// Fresh Fruit Cup
+// Yogurt Parfait
+// Hash Browns
+// BEVERAGES
+
+// Freshly Squeezed Orange Juice
+// Gourmet Coffee (Hot/Iced)
+// Specialty Teas
+// Milkshakes (Vanilla, Chocolate, Strawberry)
+// Soft Drinks

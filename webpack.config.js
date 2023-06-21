73f26b392b3405f35cb0ce5f1511458874a62f9f
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-06-21 10:34:45 -0600

    trying to incorporate the output management which means I have to recreate a few things to get my site working again.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -6,7 +7,13 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Restaurant',
+    }),
+  ],
   module: {
     rules: [
       {

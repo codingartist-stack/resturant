4b957fe28d71f89dfd59744de7927a91401eb627
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-06-21 18:17:14 -0600

    moving the code that creates elements to the index js file.

--- src/index.js
+++ src/index.js
@@ -11,3 +11,27 @@ document.body.appendChild(header);
 const nav = document.createElement('nav');
 nav.classList.add('navigation');
 header.appendChild(nav);
+
+const logo = document.createElement('div');
+logo.classList.add('mainLogo');
+logo.classList.add('selected');
+logo.textContent = `Willow's Waffles`;
+nav.appendChild(logo);
+
+const location = document.createElement('div');
+location.classList.add('tab');
+location.id = 'locationTab';
+location.textContent = 'Hours and Locations';
+nav.appendChild(location);
+
+const menu = document.createElement('div');
+menu.classList.add('tab');
+menu.id = 'menuTab';
+menu.textContent = 'Menu';
+nav.appendChild(menu);
+
+const about = document.createElement('div');
+about.classList.add('tab');
+about.id = 'aboutTab';
+about.textContent = 'About';
+nav.appendChild(about);

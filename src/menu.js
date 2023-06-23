5e0d1f1a5ed45fbec337961cffd3716d7d271854
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-06-23 09:17:48 -0600

    added the rest of the menu to the menu inner HTML.

--- src/menu.js
+++ src/menu.js
@@ -15,12 +15,12 @@ export default function addMenu() {
   contentContainer.appendChild(menuTitleImage);
   contentContainer.appendChild(menuContent);
   menuContent.innerHTML = `
-  <h3>BREAKFAST WAFFLES</h3>
+<h3>BREAKFAST WAFFLES</h3>
 
 <h4>Classic Waffle</h4>
-<p>Freshly baked golden waffle served with butter and maple syrup.<p> 
+<p>Freshly baked golden waffle served with butter and maple syrup.</p> 
 <h4>Belgian Waffle</h4>
-<p>Light and fluffy Belgian waffle topped with fresh berries and whipped cream.<p>
+<p>Light and fluffy Belgian waffle topped with fresh berries and whipped cream.</p>
 <h4>Chocolate Lover's Waffle</h4>
 <p>Decadent chocolate-infused waffle drizzled with chocolate sauce, sprinkled with powdered sugar, and served with a side of vanilla ice cream.</p>
 <h4>Cinnamon Roll Waffle</h4>
@@ -28,43 +28,48 @@ export default function addMenu() {
 <h4>Fruity Blast Waffle</h4>
 <p>Waffle loaded with a medley of seasonal fruits, such as strawberries, blueberries, and bananas, accompanied by a dollop of Greek yogurt and a drizzle of honey.</p>
 <h4>Savory Breakfast Waffle</h4>
-<p>Savory waffle topped with scrambled eggs, crispy bacon or sausage, cheddar cheese, and a touch of hot sauce.</p>`;
+<p>Savory waffle topped with scrambled eggs, crispy bacon or sausage, cheddar cheese, and a touch of hot sauce.</p>
+
+<h3>SWEET CREATIONS</h3>
+
+<h4Nutella Paradise Waffle</h4> 
+<p>Irresistible waffle smeared with Nutella, sliced bananas, and crushed hazelnuts, finished with a dusting of powdered sugar.</p>
+<h4>Red Velvet Waffle</h4> 
+<p>Rich red velvet waffle with cream cheese frosting, garnished with chocolate shavings.</p>
+<h4>S'mores Waffle</h4> 
+<p>Toasted marshmallows, graham cracker crumbs, and melted chocolate drizzled over a warm waffle, reminiscent of the classic campfire treat.</p>
+<h4>Apple Pie Waffle</h4> 
+<p>Waffle infused with apple pie spices, topped with warm cinnamon apples, a scoop of vanilla ice cream, and caramel sauce.</p>
+<h4>Peanut Butter & Jelly Waffle</h4> 
+<p>Waffle spread with creamy peanut butter and your choice of strawberry or grape jelly, served with a sprinkle of crushed peanuts.</p>
+
+<h3>SAVORY DELIGHTS</h3>
+
+<h4>Chicken & Waffles</h4> 
+<p>Crispy fried chicken served with a fluffy waffle and a side of maple syrup or savory gravy.</p>
+<h4>Waffle Benedict </h4>
+<p>Poached eggs, ham or smoked salmon, and hollandaise sauce served atop a savory waffle.</p>
+<h4>Caprese Waffle</h4>
+<p>Tomato, fresh mozzarella, and basil layered on a savory waffle, drizzled with balsamic glaze.</p>
+<h4>BBQ Pulled Pork Waffle</h4> 
+<p>Tender pulled pork smothered in tangy barbecue sauce, served on a waffle with coleslaw and pickles.</p>
+
+<h3>SIDES<h3>
+
+<h4>Crispy Bacon</h4>
+<h4>Scrambled Eggs</h4>
+<h4>Fresh Fruit Cup</h4>
+<h4>Yogurt Parfait</h4>
+<h4>Hash Browns</h4>
+
+<h3>BEVERAGES</h3>
+
+<h4>Freshly Squeezed Orange Juice</h4>
+<h4>Gourmet Coffee (Hot/Iced)</h4>
+<h4>Specialty Teas</h4>
+<h4>Milkshakes (Vanilla, Chocolate, Strawberry)</h4>
+<h4>Soft Drinks</h4>
+`;
 
   return menuContent.innerHTML;
 }
-
-// BREAKFAST WAFFLES
-
-// Classic Waffle - Freshly baked golden waffle served with butter and maple syrup.
-// Belgian Waffle - Light and fluffy Belgian waffle topped with fresh berries and whipped cream.
-// Chocolate Lover's Waffle - Decadent chocolate-infused waffle drizzled with chocolate sauce, sprinkled with powdered sugar, and served with a side of vanilla ice cream.
-// Cinnamon Roll Waffle - Mouthwatering waffle infused with cinnamon and topped with cream cheese frosting and caramelized pecans.
-// Fruity Blast Waffle - Waffle loaded with a medley of seasonal fruits, such as strawberries, blueberries, and bananas, accompanied by a dollop of Greek yogurt and a drizzle of honey.
-// Savory Breakfast Waffle - Savory waffle topped with scrambled eggs, crispy bacon or sausage, cheddar cheese, and a touch of hot sauce.
-// SWEET CREATIONS
-
-// Nutella Paradise Waffle - Irresistible waffle smeared with Nutella, sliced bananas, and crushed hazelnuts, finished with a dusting of powdered sugar.
-// Red Velvet Waffle - Rich red velvet waffle with cream cheese frosting, garnished with chocolate shavings.
-// S'mores Waffle - Toasted marshmallows, graham cracker crumbs, and melted chocolate drizzled over a warm waffle, reminiscent of the classic campfire treat.
-// Apple Pie Waffle - Waffle infused with apple pie spices, topped with warm cinnamon apples, a scoop of vanilla ice cream, and caramel sauce.
-// Peanut Butter & Jelly Waffle - Waffle spread with creamy peanut butter and your choice of strawberry or grape jelly, served with a sprinkle of crushed peanuts.
-// SAVORY DELIGHTS
-
-// Chicken & Waffles - Crispy fried chicken served with a fluffy waffle and a side of maple syrup or savory gravy.
-// Waffle Benedict - Poached eggs, ham or smoked salmon, and hollandaise sauce served atop a savory waffle.
-// Caprese Waffle - Tomato, fresh mozzarella, and basil layered on a savory waffle, drizzled with balsamic glaze.
-// BBQ Pulled Pork Waffle - Tender pulled pork smothered in tangy barbecue sauce, served on a waffle with coleslaw and pickles.
-// SIDES
-
-// Crispy Bacon
-// Scrambled Eggs
-// Fresh Fruit Cup
-// Yogurt Parfait
-// Hash Browns
-// BEVERAGES
-
-// Freshly Squeezed Orange Juice
-// Gourmet Coffee (Hot/Iced)
-// Specialty Teas
-// Milkshakes (Vanilla, Chocolate, Strawberry)
-// Soft Drinks

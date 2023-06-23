c9b1a502fbfabca738f84fa769b666fd1cc99909
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-06-23 14:27:16 -0600

    added a google map iframe and embedded the content for lacations and hours into another div so I could use grid for the layout.

--- src/hoursLocations.js
+++ src/hoursLocations.js
@@ -8,6 +8,14 @@ hourLocationHero.innerHTML = `
 <p>Image by <a href="https://pixabay.com/users/stocksnap-894430/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=2594768">StockSnap</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=2594768">Pixabay</a></p>
 `;
 
+const mapInformationContainer = document.createElement('div');
+mapInformationContainer.id = 'mapInformationGrid';
+
+const mapContainer = document.createElement('div');
+mapContainer.classList.add('map');
+mapContainer.innerHTML = `
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d211821.50211046616!2d-118.3988870766095!3d33.948566494599234!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2c9a534788b73%3A0x467109a5a72d929c!2sMorningside%20Waffle%20Bar!5e0!3m2!1sen!2sus!4v1687551369579!5m2!1sen!2sus" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`;
+
 const locationContent = document.createElement('div');
 locationContent.id = 'location';
 locationContent.innerHTML = `
@@ -39,7 +47,10 @@ locationContent.innerHTML = `
 <p>Saturday and Sunday: 8:00 AM - 10:00 PM</p>
 `;
 
+mapInformationContainer.appendChild(locationContent);
+mapInformationContainer.appendChild(mapContainer);
+
 export default function addLocationsHours() {
   contentContainer.appendChild(hourLocationHero);
-  contentContainer.appendChild(locationContent);
+  contentContainer.appendChild(mapInformationContainer);
 }

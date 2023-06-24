4315abe5b41611734fee9707b94048602a64a2cc
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-06-23 22:05:07 -0600

    added the information for the image to give the artist credit.

--- src/homePage.js
+++ src/homePage.js
@@ -6,6 +6,7 @@ homePageHero.classList.add('hero');
 homePageHero.innerHTML = `
 <div class='heroLogo'>Willow's Waffles</div>
 <h4>Indulge in Waffle Wonderland at Willow's Waffles!</h4>
+<p>Image by <a href="https://pixabay.com/users/congerdesign-509903/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=1177526">congerdesign</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=1177526">Pixabay</a></p>
 `;
 
 export default function addHomePage() {

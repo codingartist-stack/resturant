8bfd5e1625279d23b1771d71f65b2524f90ebd5c
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-06-23 18:22:04 -0600

    added a file for the about page.

---
+++ src/about.js
@@ -0,0 +1,18 @@
+import { contentContainer } from './header';
+
+const aboutHero = document.createElement('div');
+aboutHero.classList.add(hero);
+aboutHero.ad = 'aboutImage';
+aboutHero.innerHTML = `
+    <h1>About Us</h1>
+    <p></p>
+`;
+
+const aboutContent = document.createElement('div');
+aboutContent.id = "about";
+aboutContent.innerText = 'Introducing Willow, a passionate and creative individual who embarked on a culinary adventure by opening her very own waffle shop called Willow's Waffles. With a lifelong love for breakfast delights and a keen eye for flavor combinations, Willow's dream was to create a space where people could experience the joy and indulgence of delicious waffles. Guided by her innovative spirit and dedication to quality, she meticulously crafted a menu that showcases a tantalizing array of sweet and savory waffle creations, each one a testament to her unwavering commitment to culinary excellence. Willow's Waffles is not just a place to enjoy mouthwatering waffles; it's a haven where customers can embrace their love for extraordinary flavors and feel the warmth of a welcoming atmosphere. Willow's passion for creating unforgettable waffle experiences shines through in every bite, leaving patrons craving more and ensuring that Willow's Waffles has become a beloved destination for waffle enthusiasts near and far.';
+
+export default function addAbout() {
+    contentContainer.appendChild(aboutHero);
+    contentContainer.appendChild(aboutContent);
+};
